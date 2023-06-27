5b4b74156b7d67b50688f8a76de45e7b5fdc235d
Adetunji Adeyimika <adadet@taltech.ee>
2023-06-28 02:07:40 +0300

    Tests DOne

--- print_char.c
+++ print_char.c
@@ -1,1 +1,16 @@
+#include "print_header.h"
+
+/**
+ * print_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int print_char(va_list val)
+{
+    char s;
+
+    s = va_arg(val, int);
+    _putchar(s);
+    return (1);
+}
 
